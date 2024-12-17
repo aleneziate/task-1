de29ade42eae8d8c2112e2c3ce7b4db8f35b364f
alene <alene@ABDALLAH>
2024-12-17 08:28:53 +0300

    Add project files.

---
+++ coded task/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.Design;
+
+int num1 = 5;
+double num2 = 5.5;
+    string FirstName = "ali";
+bool isTrue = true;
+Console.WriteLine(num1 + "," + num2 + "," + FirstName + "," +isTrue);
+
+
+// task 2
+
+
+int number = 5;
+double result = Convert.ToDouble(number);
+//the convert dosent chage it in the data type
+
+
+
+//variabels 
+
+
+
+string stdentName = "ali";
+double grade_point_average = 85.5;
+string favoriteSubject = "math";
+
+
+
+// task 2
+
+
+double width = 5.5; 
+double height = 3.2;
+double area = width * height ;
+Console.WriteLine("area of the rectangle "+ area);
+
+
+
+// challenge 1 
+
+
+Console.WriteLine("Enter hour");
+string lunch_hour = Console.ReadLine(); 
+int time_for_lunch = Convert .ToInt32(lunch_hour);
+if (time_for_lunch == 11 && time_for_lunch == 14) ;
+{
+    Console.WriteLine("its lunch time");
+
+
+
+    else
+        Console.WriteLine("not lunch time yet");
